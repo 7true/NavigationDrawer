20089279d236e9359a25f28b833911409de01efa
alx <37true@gmail.com>
2018-12-08 19:35:49 +0700

    fragments added

--- app/src/main/java/tk/alltrue/navigationdrawer/MainActivity.java
+++ app/src/main/java/tk/alltrue/navigationdrawer/MainActivity.java
@@ -2,9 +2,10 @@ package tk.alltrue.navigationdrawer;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,7 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -79,13 +80,17 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Fragment fragment = null;
+        Class fragmentClass = null;
+
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-            Toast.makeText(getApplicationContext(), "you chose camera", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), "you chose camera", Toast.LENGTH_SHORT).show();
+            fragmentClass = FirstFragment.class;
         } else if (id == R.id.nav_gallery) {
-
+            fragmentClass = SecondFragment.class;
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -95,6 +100,16 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+        item.setChecked(true);
+        setTitle(item.getTitle());
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
